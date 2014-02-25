2e167763cda5b8164a9eb18d2360d613cf0fd517
Jesse Stuart <jesse@jessestuart.ca>
2014-02-24 21:53:26 -0500

    Fix selected nav item calculation: Allow overlapping URLs

--- assets/internal/nav.js.jsx
+++ assets/internal/nav.js.jsx
@@ -5,10 +5,40 @@
 	AppBar.Views.Nav = React.createClass({
 		displayName: "AppBar.Views.Nav",
 
+		getInitialState: function () {
+			return {
+				selectedURL: null
+			};
+		},
+
 		getDefaultProps: function () {
 			return {
 				items: []
 			};
+		},
+
+		componentWillMount: function () {
+			this.__calcSelectedItem(this.props.items);
+		},
+
+		componentWillReceiveProps: function (props) {
+			if (props.items && props.items !== this.props.items) {
+				this.__calcSelectedItem(props.items);
+			}
+		},
+
+		__calcSelectedItem: function (items) {
+			var selectedURL = null;
+			items.forEach(function (item) {
+				if (document.referrer.match(new RegExp("^"+ item.url))) {
+					if (selectedURL === null || item.url.length > selectedURL.length) {
+						selectedURL = item.url;
+					}
+				}
+			});
+			this.setState({
+				selectedURL: selectedURL
+			});
 		},
 
 		render: function () {
@@ -18,9 +48,10 @@
 						key={item.iconName}
 						iconName={item.iconName}
 						title={item.title}
-						url={item.url} />
+						url={item.url}
+						selected={item.url === this.state.selectedURL} />
 				);
-			});
+			}.bind(this));
 
 			return (
 				<nav>
@@ -41,10 +72,8 @@
 		},
 
 		render: function () {
-			var selected = document.referrer.match(new RegExp("^"+ this.props.url));
-
 			return (
-				<li className={this.props.iconName + (selected ? " active" : "")}>
+				<li className={this.props.iconName + (this.props.selected ? " active" : "")}>
 					<a href={this.props.url} title={this.props.title} onClick={this.handleClick}>
 						<span>{this.props.title}</span>
 					</a>
